404c62df676ecb1c1a7812a8bb14a54d6efabf9a
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-03-31 10:06:31 +0100

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,9 +2,7 @@
 """ More involved type annotations  """
 from typing import Mapping, Any, Sequence, Union, TypeVar
 
-
 T = TypeVar('T')
-
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None
