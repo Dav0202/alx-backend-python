fedc3e66f045600ee5c21a2125d173bed8ef3d04
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-04-04 12:38:44 +0100

    Update 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
 
+""" Async Comprehensions """
+
+from asyncio import gather
+from time import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ Four parallel comprehensions Run time """
+    start = time()
+    task_array = [async_comprehension() for i in range(4)]
+    await gather(*task_array)
+    end = time()
+    return (end - start)
